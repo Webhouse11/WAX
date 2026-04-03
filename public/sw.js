245855f0f870217c9074d8b59beb41360b3a50ea
Webhouse11 <olurantiprofile@gmail.com>
2026-04-03 10:18:13 -0700

    feat: Update site title and ad zone
    - Changes the browser tab title to "WAX KIDS - AI Insights & Future Trends".
    - Updates the ad zone ID in the service worker to `10827219`.
    - Adds a script tag for ad tracking with a zone ID of `226199`.

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 10827188
+    "zoneId": 10827219
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
